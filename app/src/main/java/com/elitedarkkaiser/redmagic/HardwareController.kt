032004a26a1970cef2d698b3a6cc5c2daaf50c14
austineyoung2000 <austineyoung2000@gmail.com>
2026-04-21 04:29:34 -0400

    Fix duplicate/broken fan LED functions part 2

--- app/src/main/java/com/elitedarkkaiser/redmagic/HardwareController.kt
+++ app/src/main/java/com/elitedarkkaiser/redmagic/HardwareController.kt
@@ -95,6 +95,7 @@ object HardwareController {
 
 
     
+    
     fun setFanLedEnabled(enabled: Boolean): Boolean {
         return if (enabled) {
             RootShell.exec("echo 0x3002005 > $LED_EFFECT; echo 1 > $LED_CFG")

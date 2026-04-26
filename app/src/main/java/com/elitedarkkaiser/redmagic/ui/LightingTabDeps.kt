f73baf775041dc5613be0732d3a3d4148fa11456
austineyoung2000 <austineyoung2000@gmail.com>
2026-04-26 06:55:10 -0400

    Add Lighting tab dependency contract

---
+++ app/src/main/java/com/elitedarkkaiser/redmagic/ui/LightingTabDeps.kt
@@ -0,0 +1,29 @@
+package com.elitedarkkaiser.redmagic.ui
+
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+
+data class LightingTabDeps(
+    val scrollTabContainer: () -> LinearLayout,
+    val sectionPanel: () -> LinearLayout,
+    val sectionHeader: (String, String) -> LinearLayout,
+    val bodyText: (String) -> TextView,
+    val subtleLabel: (String) -> TextView,
+    val infoRow: (String, TextView) -> LinearLayout,
+    val actionButton: (String, Boolean, () -> Unit) -> Button,
+    val singleRow: (Button) -> LinearLayout,
+    val row: (Button, Button) -> LinearLayout,
+    val dp: (Int) -> Int,
+
+    val getRealTimePreviewEnabled: () -> Boolean,
+    val setRealTimePreviewEnabled: (Boolean) -> Unit,
+    val saveRealTimePreviewEnabled: (Boolean) -> Unit,
+
+    val showFanLedDialog: () -> Unit,
+    val showLogoLedDialog: () -> Unit,
+    val showShoulderLedDialog: () -> Unit,
+    val showGameModeAppPicker: () -> Unit,
+    val showGameModeProfileDialog: () -> Unit,
+    val gameModeAppsSummary: () -> String
+)
